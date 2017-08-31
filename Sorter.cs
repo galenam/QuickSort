d577e0910ef96d59c1dd2d43e8b21f0516af6231
gmelnik <Galina.Melnik@homecredit.ru>
2017-08-31 08:16:26 +0300

    add code of define pivot

--- Sorter.cs
+++ Sorter.cs
@@ -1,9 +1,57 @@
-namespace Name
-{
-    
+using System.Linq;
+using System.Collection;
 
-public class QuickSort
-{
+namespace QuickSort
+{ 
+	public class Sorter
+	{
+		public IEnumerable<int> ArrayToSort {get;set;}
+		public Sorter (IEnumerable<int> _array)
+		{
+			ArrayToSort = _array;
+		}
 
-}
+		public IEnumerable<int> Sort()
+		{
+			if (ArrayToSort==null || !ArrayToSort.Any() ||ArrayToSort.Take(2).Count == 1 )
+			{
+				return ArrayToSort;
+			}
+
+			var length = ArrayToSort.Count();
+			var pivot = ArrayToSort[length/2];
+			var indexBegin=0, indexEnd=length-1;
+
+// todo : подумать, что делать с циклом, зачем он мне тут. Объединить if-ы. Поставить ограничения и выход из цикла по достижению середины
+			for (int i = 0; i < length; i++)
+			{
+				if (ArrayToSort[indexBegin]> pivot && ArrayToSort[indexEnd]< pivot)
+				{
+					var tempElement = ArrayToSort[indexBegin];
+					ArrayToSort[indexBegin] = ArrayToSort[indexEnd];
+					ArrayToSort[indexEnd] = tempElement;
+					indexBegin++;
+					indexEnd--;
+				}
+
+				if (ArrayToSort[indexBegin]<= pivot && ArrayToSort[indexEnd] > pivot)
+				{
+					indexBegin++;
+					indexEnd--;
+				}
+
+				if (ArrayToSort[indexBegin]>pivot && ArrayToSort[indexEnd] > pivot)
+				{
+					indexEnd--;
+				}
+
+				if (ArrayToSort[indexBegin] <=pivot && ArrayToSort[indexEnd] < pivot)
+				{
+					indexBegin++;
+				}
+			}
+
+			return Enumerable.Empty<int>();
+		}
+	}
 }
